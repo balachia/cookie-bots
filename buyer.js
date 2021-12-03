0f9ab2387b822ca0d04be536e37d270ec454ad5b
Tony Vashevko <avashevko@gmail.com>
2021-12-03 15:54:39 +0800

    init

---
+++ buyer.js
@@ -0,0 +1,34 @@
+if(BuyBot === undefined) var BuyBot = {};
+if(typeof CCSE == 'undefined') Game.LoadMod('https://klattmose.github.io/CookieClicker/CCSE.js');
+BuyBot.name = 'BuyBot';
+BuyBot.version = '0.0.1';
+BuyBot.GameVersion = '2.031';
+
+BuyBot.launch = function(){
+	/**
+	
+	All the code you want to delay goes here
+	Put "MyMod.isLoaded = 1;" somewhere within
+	
+	**/
+        BuyBot.init = function() {
+            Game.customStatsMenu.push(function(){
+                CCSE.AppendStatsVersionNumber(BuyBot.name, BuyBot.version);
+            });
+
+            BuyBot.isLoaded = 1;
+        }
+
+        if(CCSE.ConfirmGameVersion(BuyBot.name, BuyBot.version, BuyBot.GameVersion)) BuyBot.init();
+}
+
+if(!BuyBot.isLoaded){
+	if(CCSE && CCSE.isLoaded){
+		BuyBot.launch();
+	}
+	else{
+		if(!CCSE) var CCSE = {};
+		if(!CCSE.postLoadHooks) CCSE.postLoadHooks = [];
+		CCSE.postLoadHooks.push(BuyBot.launch);
+	}
+}
